6cb5b6cb12540181391664f9bc0e7b8ef0a5a0da
Altaf Ali <73736955+Altafali11@users.noreply.github.com>
2022-07-22 11:56:15 +0530

    Add files via upload

---
+++ Sum_ difference_product_.java
@@ -0,0 +1,15 @@
+public class Sum_difference_product{
+
+    public static void main(String[] args) {
+	int a = 10;
+    int b=  5;
+    int sum = a+b;
+    int product = a*b;
+    int division = a/b;
+    float average =  sum/2f;
+        System.out.println("The sum of two number is "  +sum );
+        System.out.println("The product of two number is " +product );
+        System.out.println("The division of two number is " +division);
+        System.out.println("The average of two number is " +average);
+    }
+}
