9df84d5988a1f711a9959c8c477094fd2d052d23
Altaf Ali <73736955+Altafali11@users.noreply.github.com>
2022-07-22 12:15:33 +0530

    Add files via upload

---
+++ Celsius_to_fahrenheit.java
@@ -0,0 +1,7 @@
+public class Celsius_to_fahrenheit {
+    public static void main(String[] args) {
+        double celsius = 100;
+        double  formula = 1.80*celsius+32;
+        System.out.println(" convert celsius to fahrenheit " + formula);
+        }
+    }
