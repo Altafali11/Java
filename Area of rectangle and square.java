51fc8d60a21e1498378ccab2d55ede4b97f40fc5
Altaf Ali <73736955+Altafali11@users.noreply.github.com>
2022-07-22 12:58:35 +0530

    Add files via upload

---
+++ Area of rectangle and square.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Area_rectangle_square {
+    public static void main(String[] args) {
+        System.out.println("Area of rectangle");
+        int length =  5;
+        int width  =  7;
+        int area   =  length*width;
+        System.out.println(area);
+
+        System.out.println("Area of square");
+        int side1 =  5;
+        int side2  =  5;
+        int area1    = side1*side2;
+        System.out.println(area1);
+    }
+
+
+    }
+
