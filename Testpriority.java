1d8a23a40afccaebb33baa3b51fe50e08955391e
Altaf Ali <jeeshan90ali57@gmail.com>
2023-03-01 02:06:54 +0530

    Update Testpriority.java

--- Testpriority.java
+++ Testpriority.java
@@ -1,25 +1,19 @@
 package thread;
 
-public class Testpriority extends Thread{
+class Priority extends Thread{
 	public void run() {
-		System.out.println("Thread is running");
+		System.out.println(Thread.currentThread().getPriority());
 	}
-
+}
+public class Thread_Priority {
 	public static void main(String[] args) {
-	Testpriority tp= new Testpriority();
-	Testpriority tp1= new Testpriority();
-    System.out.println("priority of thread tp is"+tp.getPriority());
-    System.out.println("priority of thread tp1 is"+tp1.getPriority());
-    tp.setPriority(6);
-    tp1.setPriority(2);
-    System.out.println("priority of thread tp is"+tp.getPriority());
-    System.out.println("priority of thread tp1 is"+tp1.getPriority());
-    System.out.println("current thread name is "+ Thread.currentThread().getName());
-    System.out.println("current thread priority value" +Thread.currentThread().getPriority());
-    Thread.currentThread().setPriority(8);
-    System.out.println("current thread is "+Thread.currentThread().getPriority());
-    Testpriority tp2 = new Testpriority();
-    System.out.println("priority of thread tp2 is "+Thread.currentThread().getPriority());
+		
+		System.out.println("Default priority  " + Thread.currentThread().getPriority());
+		Thread.currentThread().setPriority(7);
+		System.out.println("new Priority  "  + Thread.currentThread().getPriority());
+        Priority p = new Priority();
+        p.start();
 	}
 
 }
+
