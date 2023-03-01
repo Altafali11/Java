b9992dbf6a3cf20557955bdd823349a2aed5b25b
Altaf Ali <jeeshan90ali57@gmail.com>
2023-03-01 12:24:58 +0530

    Update TestSleepMethod.java

--- TestSleepMethod.java
+++ TestSleepMethod.java
@@ -1,38 +1,75 @@
 package thread;
 
-/*public class TestSleepMethod extends Thread {
-	public void run() {
-		for(int i=1;  i<5; i++) {
-			try {
-				Thread.sleep(500);
-			}
-			catch(InterruptedException e) {
-				System.out.println(e);
-			}
-			System.out.println(i);
-		}
-	}
+//  Sleep method without making thread
+//public class Demo_Sleep {
+//	public static void main(String[] args) {
+//
+//		for(int i=1; i<=10; i++) {
+//			try {
+//				Thread.sleep(1000);
+//				System.out.println(i);
+//			}
+//			catch(Exception e) {
+//				System.out.println(e);
+//			}
+//		}
+//	}
+//
+//}
+
+
+
+                        // sleep method by making thread
+
+//public class Demo_Sleep extends Thread {
+//	 public void run() {
+//		 
+//		 for(int i=1; i<=5; i++) {      
+//			 try {
+//				 Thread.sleep(1000);
+//				
+//			 }
+//			 catch(Exception e) {
+//				 System.out.println(e);
+//			 }
+//			 System.out.println(i);
+//		 }
+//	 }
+//	
+//	public static void main(String[] args) {
+//		
+//		Demo_Sleep DM = new Demo_Sleep();
+//		DM.start();	
+//	}
+//	}
+
+
+
+
+public class Demo_Sleep extends Thread {
+	 public void run() {
+		 
+		 for(int i=1; i<=5; i++) {      
+			 try {
+				 Thread.sleep(1000);
+				
+			 }
+			 catch(Exception e) {
+				 System.out.println(e);
+			 }
+			 System.out.println( i + Thread.currentThread().getName());
+		 }
+	 }
+	
 	public static void main(String[] args) {
-		TestSleepMethod  t1 = new TestSleepMethod();
-		TestSleepMethod  t2 = new TestSleepMethod();
-        t1.start();
-        t2.start();
+		
+		Demo_Sleep DM = new Demo_Sleep();
+			DM.start();     
+	//		DM.run();   // if we call run method then thread is not created and main thread running
+			
+		Demo_Sleep Dm = new Demo_Sleep();
+		Dm.start();
+	}
 	}
 
-}
-*/
-
-public  class TestSleepMethod extends Thread{
-	public static void main(String[]args) {
-		try {
-			for(int j=0; j<5;j++) {
-				Thread.sleep(1000);
-				System.out.println(j);
-			}
-		}
-			catch(InterruptedException e) {
-				System.out.println(e);
-			}
-		}
-	}
 
